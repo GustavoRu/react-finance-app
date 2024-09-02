f39c1ef28e4f91fa50631f470953ebdfb1916021
GustavoRu <gustavoruiz.gr96@gmail.com>
2024-09-01 23:22:37 -0300

    add versel.json

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -15,7 +15,6 @@ export default function Home(props) {
       })
       .then((data) => {
         setDataDollars(data);
-        console.log("data", data);
         setLoading(false);
       })
       .catch((error) => {
